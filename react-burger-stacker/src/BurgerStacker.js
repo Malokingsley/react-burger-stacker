0eb2179f3b00be2553cb1e83fad17062af2cabfc
MalcolmKemp <Malokingsley@icloud.com>
2023-02-13 11:34:49 -0500

    'hw attempted'

---
+++ react-burger-stacker/src/BurgerStacker.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const BurgerStacker = () => {
+  state = {
+    Ingredients
+  }
+  
+    return (
+    <div>BurgerStacker</div>
+  )
+}
+
+export default BurgerStacker
